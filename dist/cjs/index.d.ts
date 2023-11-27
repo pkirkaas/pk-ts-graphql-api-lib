eeee88c09b8522c6714546921ddcb4b30255b478
Paul Kirkaas <pkirkaas@gmail.com>
2023-11-27 23:17:00 +0100

    Updated node-lib

--- dist/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Entry point for API Express functions & settings
- */
-export * from './lib.js';
-//# sourceMappingURL=index.d.ts.map
--- dist/cjs/lib.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Library of useful, general Express API functions & initializations
- */
-import 'express-async-errors';
-export declare const app: any;
-export declare const apiRouter: any;
-//# sourceMappingURL=lib.d.ts.map
